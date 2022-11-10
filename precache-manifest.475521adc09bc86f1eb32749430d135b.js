8cbd920df32ecb468d5887161b80bb23fdf170cb
RosieCummingCPT <roseinthecape@outlook.com>
2022-11-10 21:15:08 +0200

    Updates

---
+++ precache-manifest.475521adc09bc86f1eb32749430d135b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e5363f63d8b0c4ef5261fb1b5e671ae",
+    "url": "/me/index.html"
+  },
+  {
+    "revision": "59f57fad2a47ae6fa709",
+    "url": "/me/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "de7ec3d412301402c55b",
+    "url": "/me/static/js/2.cb4cfa25.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/me/static/js/2.cb4cfa25.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59f57fad2a47ae6fa709",
+    "url": "/me/static/js/main.191fdd76.chunk.js"
+  },
+  {
+    "revision": "43e1106f74f1c139955e",
+    "url": "/me/static/js/runtime-main.8e474a00.js"
+  }
+]);
